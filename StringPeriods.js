027ec062a682abdf7aee18adb2f2b04a656d235f
d0ppler1977 <david@linknordic.com>
2018-12-31 11:45:22 +0100

    Update StringPeriods.js

--- StringPeriods.js
+++ StringPeriods.js
@@ -1,8 +1,8 @@
 function StringPeriods(str) {
-    const max = parseInt(str.length / 2, 10);
+    const max = parseInt(str.length / 2, 10); // max is the max length of the substring, it can never be more than half of the original
     const l = str.length;
-    for (i = max; i > 0; i --) {
-        for (j = 0; j <= l - i; j ++) {
+    for (i = max; i > 0; i --) { // i is the length of the substring
+        for (j = 0; j <= l - i; j ++) { // j is the start position of the substring
             const substring = str.substring(j, j + i);
             const count = str.split(substring).length - 1;
             //console.log("found " + count + " occurences of " + substring + " in " + str);
